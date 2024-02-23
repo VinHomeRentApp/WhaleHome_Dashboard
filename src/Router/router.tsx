d223984eaa55795e5494c8a4d20d8d5ea1d76442
Tran Pham Thanh Nam <trannam.nt865@gmail.com>
2024-02-23 10:21:05 +0700

    .

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import DashBoard from '../Pages/Dashboard'
 import UserPage from '../Pages/UserPage'
 import Login from '../Pages/Login'
