860b7bfbc47464939486d02183760d344b09f675
Nguyễn Trung Kiên <93828844+Trungkien03@users.noreply.github.com>
2024-03-10 14:18:51 +0700

    Work/kien reformat (#31)
    * reformat code and building some icon for dashboard and reformat menu
    
    * remove unused var
    
    * review unused var
    
    * update...
    
    * refactor
    
    * fix protected routes
    
    * function login with protected routes
    
    * add login toast and logout function
    
    * remove unused var
    
    * add loading
    
    ---------
    
    Co-authored-by: Tran Pham Thanh Nam <trannam.nt865@gmail.com>

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,15 +1,17 @@
 import { FileTextOutlined, HomeFilled, SolutionOutlined, UserOutlined } from '@ant-design/icons'
-import { Space } from 'antd'
+import { Space, Spin } from 'antd'
 import React, { useEffect } from 'react'
 import DashboardCard from '../Components/Dashboard/DashboardCard'
 import DashboardChart from '../Components/Dashboard/DashboardChar'
 import RecentPost from '../Components/RecentPost'
-import { useAppDispatch } from '../redux/containers/store'
+import { RootState, useAppDispatch } from '../redux/containers/store'
 import { setUser } from '../redux/slices/auth.slice'
 import { User } from '../types/user.type'
+import { useSelector } from 'react-redux'
 
 const DashBoard: React.FC = () => {
   const dispatch = useAppDispatch()
+  const { isLoading } = useSelector((state: RootState) => state.auth)
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem('user') || 'null') as User
@@ -18,76 +20,78 @@ const DashBoard: React.FC = () => {
 
   return (
     <div>
-      <Space direction='horizontal' style={{ width: '100%' }}>
-        <DashboardCard
-          icon={
-            <FileTextOutlined
-              style={{
-                color: 'green',
-                backgroundColor: 'rgba(0,255,0,0.25)',
-                borderRadius: 20,
-                fontSize: 24,
-                padding: 8
-              }}
-            />
-          }
-          title={'Post'}
-          URL='/post/count-all'
-        />
+      <Spin spinning={isLoading}>
+        <Space direction='horizontal' style={{ width: '100%' }}>
+          <DashboardCard
+            icon={
+              <FileTextOutlined
+                style={{
+                  color: 'green',
+                  backgroundColor: 'rgba(0,255,0,0.25)',
+                  borderRadius: 20,
+                  fontSize: 24,
+                  padding: 8
+                }}
+              />
+            }
+            title={'Post'}
+            URL='/post/count-all'
+          />
 
-        <DashboardCard
-          icon={
-            <UserOutlined
-              style={{
-                color: 'purple',
-                backgroundColor: 'rgba(0,255,255,0.25)',
-                borderRadius: 20,
-                fontSize: 24,
-                padding: 8
-              }}
-            />
-          }
-          title={'Customer'}
-          URL='/user/count-all'
-        />
+          <DashboardCard
+            icon={
+              <UserOutlined
+                style={{
+                  color: 'purple',
+                  backgroundColor: 'rgba(0,255,255,0.25)',
+                  borderRadius: 20,
+                  fontSize: 24,
+                  padding: 8
+                }}
+              />
+            }
+            title={'Customer'}
+            URL='/user/count-all'
+          />
 
-        <DashboardCard
-          icon={
-            <HomeFilled
-              style={{
-                color: 'black',
-                backgroundColor: 'rgba(0,255,255,0.25)',
-                borderRadius: 20,
-                fontSize: 24,
-                padding: 8
-              }}
-            />
-          }
-          title={'Appartment'}
-          URL='/apartments/count-all'
-        />
+          <DashboardCard
+            icon={
+              <HomeFilled
+                style={{
+                  color: 'black',
+                  backgroundColor: 'rgba(0,255,255,0.25)',
+                  borderRadius: 20,
+                  fontSize: 24,
+                  padding: 8
+                }}
+              />
+            }
+            title={'Appartment'}
+            URL='/apartments/count-all'
+          />
 
-        <DashboardCard
-          icon={
-            <SolutionOutlined
-              style={{
-                color: 'black',
-                backgroundColor: 'rgba(0,255,255,0.25)',
-                borderRadius: 20,
-                fontSize: 24,
-                padding: 8
-              }}
-            />
-          }
-          title={'Contract'}
-          URL='/contracts/count-all'
-        />
-      </Space>
+          <DashboardCard
+            icon={
+              <SolutionOutlined
+                style={{
+                  color: 'black',
+                  backgroundColor: 'rgba(0,255,255,0.25)',
+                  borderRadius: 20,
+                  fontSize: 24,
+                  padding: 8
+                }}
+              />
+            }
+            title={'Contract'}
+            URL='/contracts/count-all'
+          />
+        </Space>
 
-      <DashboardChart />
-      <div>
-        <RecentPost />
-      </div>
+        <DashboardChart />
+        <div>
+          <RecentPost />
+        </div>
+      </Spin>
     </div>
   )
 }
--- src/redux/slices/auth.slice.ts
+++ src/redux/slices/auth.slice.ts
@@ -3,11 +3,15 @@ import { login } from '../actions/auth.actions'
 import { initialAuthState } from '../types/auth.types'
 
 const authSlice = createSlice({
-  name: 'user',
+  name: 'auth',
   initialState: initialAuthState,
   reducers: {
     setUser: (state, action) => {
       state.userInfo = action.payload
+    },
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload
+      console.log(state.isLoading)
     },
     logout: (state) => {
       localStorage.removeItem('userToken')
@@ -31,80 +35,6 @@ const authSlice = createSlice({
   }
 })
 
-export const { logout, setUser } = authSlice.actions
+export const { logout, setUser, setIsLoading } = authSlice.actions
 
 export default authSlice.reducer
-
-// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-// import { User } from '../types/user.type'
-// import { http } from '../utils/http'
-// import { LoginResponse, ResponseSuccessful } from '../types/response.type'
-
-// export interface initialState {
-//   currentUser?: User
-//   isLoading: boolean
-//   jwt: string
-// }
-
-// const initialState: initialState = {
-//   currentUser: undefined,
-//   isLoading: false,
-//   jwt: ''
-// }
-
-// export interface LoginArgs {
-//   email: string
-//   password: string
-// }
-
-// export const login = createAsyncThunk('auth/login', async ({ email, password }, thunkAPI) => {
-//   try {
-//     const response = await http.post('/auth/authenticate', {
-//       email: email,
-//       password: password
-//     })
-//     return response.data.data.access_token
-//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   } catch (err: any) {
-//     return thunkAPI.rejectWithValue(err.response.data.errors)
-//   }
-// })
-
-// const authSlice = createSlice({
-//   name: 'auth',
-//   initialState,
-//   reducers: {}, // Add an empty reducers property
-//   extraReducers: (builder) =>
-//     builder
-//       .addCase(login.pending, (state) => {
-//         state.isLoading = true
-//       })
-//       .addCase(login.fulfilled, (state, action) => {
-//         state.isLoading = false
-//         state.jwt = action.payload.access_token
-//       })
-//       .addCase(login.rejected, (state) => {
-//         state.isLoading = false
-//       })
-//   // .addCase(getCurrentUser.pending, (state) => {
-//   //   state.isLoading = true
-//   // })
-//   // .addCase(getCurrentUser.fulfilled, (state, action) => {
-//   //   state.isLoading = false
-//   //   state.currentUser = action.payload
-//   // })
-//   // .addCase(getCurrentUser.rejected, (state) => {
-//   //   state.isLoading = false
-//   //   state.currentUser = null
-//   // })
-//   // .addCase(logout.fulfilled, (state) => {
-//   //   state.isLoading = false
-//   //   state.currentUser = null
-//   // })
-// })
-
-// export default authSlice.reducer
-
-// import { registerUser, userLogin } from './authActions'
-
-// initialize userToken from local storage
