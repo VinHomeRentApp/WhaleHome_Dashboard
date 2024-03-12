91c60837b405f0a6eb152c2dfb1a28740c8b3fb0
Nguyễn Trung Kiên <93828844+Trungkien03@users.noreply.github.com>
2024-03-12 11:38:35 +0700

    Work/kien reformat (#42)
    * reformat code and building some icon for dashboard and reformat menu
    
    * remove unused var
    
    * review unused var
    
    * update...
    
    * refactor
    
    * fix protected routes
    
    * function login with protected routes
    
    * add login toast and logout function
    
    * remove unused var
    
    * add loading
    
    * create add post modal
    
    * fix text button ok
    
    * refactor file area
    
    * update...
    
    * crud apartment
    
    * fix a little bit
    
    * fix unused var
    
    * fix unused var
    
    * add create post screen function
    
    * add spinning
    
    * fix unused var
    
    * fix dependency array
    
    * create edit post
    
    * update not view image in post
    
    ---------
    
    Co-authored-by: Tran Pham Thanh Nam <trannam.nt865@gmail.com>

--- src/Pages/Dashboard/PostPage/ImageViewerModal.tsx
+++ src/Pages/Dashboard/PostPage/ImageViewerModal.tsx
@@ -20,16 +20,24 @@ const ImageViewerModal: React.FC<ImageViewerModalProps> = ({ visible, imageList,
         handleClose()
       }}
     >
-      <Carousel autoplay={true} draggable arrows>
-        {imageList.map((img) => (
-          <img
-            key={img.id}
-            alt={img.image_alt}
-            src={img.image_url}
-            style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-          />
-        ))}
-      </Carousel>
+      {imageList.length === 0 ? (
+        <img
+          src='https://www.huber-online.com/daisy_website_files/_processed_/8/0/csm_no-image_d5c4ab1322.jpg'
+          alt=''
+          style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+        />
+      ) : (
+        <Carousel autoplay={true} draggable arrows>
+          {imageList.map((img) => (
+            <img
+              key={img.id}
+              alt={img.image_alt}
+              src={img.image_url}
+              style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+            />
+          ))}
+        </Carousel>
+      )}
     </Modal>
   )
 }
