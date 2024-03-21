bd55a75d5c48304eca88ca2f5842ea2267d188cc
Nguyễn Trung Kiên <93828844+Trungkien03@users.noreply.github.com>
2024-03-21 12:08:39 +0700

    change login to dark mode

--- src/Pages/Authentication/Login.tsx
+++ src/Pages/Authentication/Login.tsx
@@ -1,15 +1,15 @@
-import { Button, Form, Grid, Input, Spin, theme, Typography } from 'antd'
+import { Button, Form, Grid, Image, Input, Spin, theme } from 'antd'
 
 import { LockOutlined, MailOutlined } from '@ant-design/icons'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
+import { typoColor } from '../../constants/mainColor'
 import { RootState, useAppDispatch } from '../../redux/containers/store'
 import { handleSubmit } from '../../usecases/HandleLogin'
 
 const { useToken } = theme
 const { useBreakpoint } = Grid
-const { Text, Title } = Typography
 
 export default function Login() {
   const { token } = useToken()
@@ -21,14 +21,15 @@ export default function Login() {
   const [password, setPassword] = useState<string>('')
   const navigate = useNavigate()
 
-  const styles = {
+  type Container = React.CSSProperties | undefined
+
+  const styles: { [key: string]: Container } = {
     container: {
       margin: '0 auto',
       padding: screens.md ? `${token.paddingXL}px` : `${token.sizeXXL}px ${token.padding}px`,
-      width: '380px',
-      border: '1px solid #fff',
-      borderRadius: '12px',
-      boxShadow: '0 2px 10px rgba(0, 0, 0, 0.3)' // Add box shadow here
+      width: '25%',
+      backgroundColor: typoColor.subMainBackground
+      // Add box shadow here
     },
     footer: {
       marginTop: token.marginLG,
@@ -40,26 +41,21 @@ export default function Login() {
     },
     section: {
       alignItems: 'center',
-      backgroundColor: token.colorBgContainer,
+      backgroundColor: typoColor.subMainBackground,
       display: 'flex',
-      height: screens.sm ? '80vh' : 'auto',
-      padding: screens.md ? `${token.sizeXXL}px 0px` : '0px'
+      height: '100vh'
     },
     text: {
-      color: token.colorTextSecondary
-    },
-    title: {
-      fontSize: screens.md ? token.fontSizeHeading2 : token.fontSizeHeading3
+      color: typoColor.white1
     }
   }
 
   return (
     <Spin spinning={isLoading}>
-      <section style={styles.section}>
+      <div style={styles.section}>
         <div style={styles.container}>
-          <div style={{ textAlign: 'center', marginBottom: token.marginXL }}>
-            <Title style={styles.title}>Login</Title>
-            <Text style={styles.text}>Welcome back WhaleHome DashBoard</Text>
+          <div style={{ textAlign: 'center' }}>
+            <Image preview={false} style={{ width: '25%', height: '25%' }} src='../../../public/main-logo.png' />
           </div>
           <Form
             name='normal_login'
@@ -73,51 +69,65 @@ export default function Login() {
             layout='vertical'
             requiredMark='optional'
           >
+            <div style={{ textAlign: 'center' }}>
+              <Form.Item
+                name='email'
+                rules={[
+                  {
+                    type: 'email',
+                    required: true,
+                    message: 'Please input your Email!'
+                  }
+                ]}
+              >
+                <Input
+                  style={{ width: '80%' }}
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                  }}
+                  prefix={<MailOutlined />}
+                  placeholder='Email'
+                />
+              </Form.Item>
+              <Form.Item
+                name='password'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your Password!'
+                  }
+                ]}
+              >
+                <Input.Password
+                  style={{ width: '80%' }}
+                  onChange={(e) => {
+                    setPassword(e.target.value)
+                  }}
+                  prefix={<LockOutlined />}
+                  type='password'
+                  placeholder='Password'
+                />
+              </Form.Item>
+            </div>
             <Form.Item
-              name='email'
-              rules={[
-                {
-                  type: 'email',
-                  required: true,
-                  message: 'Please input your Email!'
-                }
-              ]}
+              style={{
+                marginBottom: '0px',
+                width: '100%',
+                textAlign: 'center'
+              }}
             >
-              <Input
-                onChange={(e) => {
-                  setEmail(e.target.value)
-                }}
-                prefix={<MailOutlined />}
-                placeholder='Email'
-              />
-            </Form.Item>
-            <Form.Item
-              name='password'
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Password!'
-                }
-              ]}
-            >
-              <Input.Password
-                onChange={(e) => {
-                  setPassword(e.target.value)
-                }}
-                prefix={<LockOutlined />}
-                type='password'
-                placeholder='Password'
-              />
-            </Form.Item>
-            {/*  */}
-            <Form.Item style={{ marginBottom: '0px' }}>
-              <Button block={true} type='primary' htmlType='submit'>
+              <Button
+                block={true}
+                type='primary'
+                style={{ color: typoColor.subMainBackground, width: '30%', fontWeight: 'initial' }}
+                htmlType='submit'
+              >
                 Log in
               </Button>
             </Form.Item>
           </Form>
         </div>
-      </section>
+      </div>
     </Spin>
   )
 }
